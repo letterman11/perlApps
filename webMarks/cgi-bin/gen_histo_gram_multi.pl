d05bdba1026cfad6803df14c2bb1a39fa54315ae
Angus Brooks <angusbrooks@gmail.com>
2021-10-23 11:39:41 -0400

    added another dropdown and edited regex to allow numbers to be looked up

--- webMarks/cgi-bin/gen_histo_gram_multi.pl
+++ webMarks/cgi-bin/gen_histo_gram_multi.pl
@@ -3,15 +3,15 @@ my $hist_sql_all_str = "select b.url, a.title, a.dateAdded from WM_BOOKMARK a, W
 
 sub gen_histogram
 {
-    my $userID = shift;
-	my %markHist = ();
+	my $userID = shift;
+        my %markHist = ();
 	my %elimdups = ();
 	my @histo_list = ();	
 	my ($title,$url,$dateAdded) = (1,0,2);
 
 	### error checking ????? ##############
 	my $sth = $main::dbh->prepare($hist_sql_all_str);
-    $sth->bind_param(1,$userID);
+        $sth->bind_param(1,$userID);
 	$sth->execute();
 	my $data_refs = $sth->fetchall_arrayref;
 	my $row_count = $sth->rows;
@@ -20,7 +20,7 @@ sub gen_histogram
 	foreach (@{$data_refs})			
 	{
 		next if not defined($_->[$title]); 
-        next if $_->[$title] =~ /^\s*$/;
+        	next if $_->[$title] =~ /^\s*$/;
 
 		if(exists($elimdups{$_->[$title]}))
 		{
@@ -40,9 +40,12 @@ sub gen_histogram
 
 		foreach (@words)
 		{
-			next if /(?:(\ba\b)|(\bfrom\b)|(\byour\b)|(\bHow\b)|(\bBE\b)|(\bas\b)|(\bthe\b)|(\bby\b)|(\bon\b)|(\band\b)|(\bis\b)|(\bFor\b)|(\bwith\b)|(\bIn\b)|(\bto\b)|(\bof\b)|(\b(\s+)\b)|(-|\+)|(\|)|[&-:><'#])/i;
+			next if /(?:(\ba\b)|(\bfrom\b)|(\byour\b)|(\bHow\b)|(\bBE\b)|(\bas\b)|(\bthe\b)|(\bby\b)|(\bon\b)|(\band\b)|(\bis\b)|(\bFor\b)|(\bwith\b)|(\bIn\b)|(\bto\b)|(\bof\b)|(\b(\s+)\b)|(-|\+)|(\|)|[&\-:><'#])/i;
+
 			next if /\b[[:cntrl:]]+\b/;
 			next if not /[\x00-\x7f]/;
+			next if length($_) < 3 && $_ =~ /\d/;
+
 			s/\s+$//g;
 			s/^\s+//g;
 			if (exists($markHist{$_}))
@@ -76,9 +79,12 @@ sub gen_optionListDiv
    my $str0;
    my $str1;
    my $str2;
+   my $str3;
    $str0 .= qq#\n\t<option value=" "> </option>#;
    $str1 .= qq#\n\t<option value=" "> </option>#;
    $str2 .= qq#\n\t<option value=" "> </option>#;
+   $str3 .= qq#\n\t<option value=" "> </option>#;
+
    for my $option (@H[0..14])
    {
        $option =~ s/\|/ /g; 
@@ -94,6 +100,11 @@ sub gen_optionListDiv
 	   $str2 .= qq#\n\t<option value="$_"> $_</option>#; 
    										} @H[30..44];
 
+   map {  
+           $_ =~ s/\|/ /g; 
+	   $str3 .= qq#\n\t<option value="$_"> $_</option>#; 
+   										} @H[45..59];
+
    my $out_hist_opts = <<"OPTION_TABLE";
        <div style="display:inline-block" id="optionDiv">
        <form>
@@ -106,6 +117,9 @@ sub gen_optionListDiv
        </select>
       <select  onchange="topOpToSearch(this.options[this.options.selectedIndex].text);" id="topOptionID" name="topOption">
           $str2 
+       </select>
+      <select  onchange="topOpToSearch(this.options[this.options.selectedIndex].text);" id="topOptionID" name="topOption">
+          $str3 
        </select>
 
        </form>
