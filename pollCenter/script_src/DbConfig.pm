b652a0c051cff4815e25ce0faea7ff5d558ae543
Angus Brooks <angusbrooks@gmail.com>
2024-03-24 06:09:53 -0400

    update Dbconfig

--- pollCenter/script_src/DbConfig.pm
+++ pollCenter/script_src/DbConfig.pm
@@ -1,6 +1,9 @@
 package DbConfig;
 
 use strict;
+our @ISA = qw(DBI);
+use DBI;
+
 use FileHandle;
 
 $::attr = { 
@@ -8,28 +11,60 @@ $::attr = {
 		RaiseError => 1,
 	}; 
 
-sub new
+$::attr2 = { 
+		PrintError => 1,
+		RaiseError => 0,
+	}; 
+
+
+####################
+# Test alias
+*DbConfig::errstr = *DBI::errstr; 
+#
+###################
+
+sub new 
 {
 	my $class = shift;
 	my %configHash = ();
-	my $self = \%configHash;
+	bless \%configHash, $class;
 
-	my $defaultConfFile = "stockDbConfig.dat";
+	my $self =  \%configHash;
+	$self->_initialize($_[0]);	
 
+	return \%configHash;
+}
+
+
+sub _initialize
+{
+	my $configHash = $_[0];
+	my $defaultConfFile = defined($_[1]) && $_[1] !~ /^\s*$/ ?  $_[1] : "stockDbConfig.dat";
  	my $fh = new FileHandle;
 	if ($fh->open("< $defaultConfFile") ) {
 		while (<$fh>) {
-			next if not /^([A-Za-z0-9]+)=([A-Za-z0-9\.]*)/; 
-			my ($key,$value) = ($1,$2);
-			$configHash{$key}=$value;
+			next if /^#/;
+			 if (/([A-Za-z0-9_]+)=([A-Za-z0-9_\-\:\.\/]*)/) 
+			{ 
+				my ($key,$value) = ($1,$2);
+				$configHash->{$key}=$value;
+		       }	
+
 		} 
 		$fh->close;
-  	} else { die "Error opening configfile \n"; }
-
-	bless ($self,$class);
-	return $self; 
+  	} else { die "Error opening configfile $defaultConfFile \n"; }
+	
 }
 
+sub _initialize2
+{
+	my $self = $_[0];
+	my %configH;
+	eval 'stockDB.pl';		
+	print STDERR "here $@ ";
+       %$self = (%$self,%configH);
+	
+}
 
 sub dbName 
 {
@@ -62,14 +97,54 @@ sub rowsPerPage
 
 }   
 
+sub dbdriver
+{
 
+	my $self = shift;
+	return $self->{'dbdriver'};
 
+}
 
+sub dbPort
+{
 
+	my $self = shift;
+	return $self->{'port'};
 
+}
 
+sub connect 
+{
+	my $self = shift;
+	my $dbdriver = $self->dbdriver; 
+	my $dbname = $self->dbName; 
+	my $dbuser = $self->dbUser; 
+	my $dbpass = $self->dbPass; 
+	my $dbhost = $self->dbHost; 
+	my $dbport = $self->dbPort; 
+      	my $dsn; 
 
+	if ($dbdriver =~ /SQLite/i) 
+	{
+		$dsn = $dbdriver ."=". $dbname;
 
+	} elsif ($dbdriver =~ /mysql/i) 
+	{
+		 $dsn = $dbdriver . ":database=$dbname;host=$dbhost;port=$dbport";	
+
+	} elsif ($dbdriver =~ /Pg/i) 
+	{
+		 $dsn = $dbdriver . ":dbname=$dbname;host=$dbhost";	
+	} else {
+
+		$dsn = $dbdriver . $dbname;
+	}
+
+    no warnings;
+	return $self->SUPER::connect($dsn,  $dbuser, $dbpass, $::attr );
+  
+
+}
 
 
 1; 
